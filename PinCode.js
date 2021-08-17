3646ebf540847203a639b25a861104de8a4d57f8
Jebakani <vjishwarya@gmail.com>
2021-08-17 21:22:04 +0530

    [JEBAKANI]ADD:Check whether pincode has 6 digit

---
+++ PinCode.js
@@ -0,0 +1,11 @@
+var prompt = require('prompt-sync')();
+let pincode=prompt('enter the pin code:');
+let regpattern=RegExp('^[1-9]{1}[0-9]{5}$');
+if(regpattern.test(pincode))
+{
+    console.log('Pincode is valid');
+}
+else
+{
+    console.log('pincode is invalid');
+}
