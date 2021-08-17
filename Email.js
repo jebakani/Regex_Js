f7cc47bdc164d3cfde85e28bae8354ea544b1d43
Jebakani <vjishwarya@gmail.com>
2021-08-17 21:56:02 +0530

    [JEBAKANI]Add:Handling the optional part

--- Email.js
+++ Email.js
@@ -1,7 +1,7 @@
 function EmailCheck(email)
 {
     //check mandatory first part
-let regpattern=RegExp('^[a-zA-Z]{3}@[a-zA-Z]+\\.[a-z]{2,3}');
+let regpattern=RegExp('^[a-zA-Z]{3}([\\- \\+ _\\.]*[a-zA-Z0-9]+)*@[a-zA-Z0-9]+\\.[a-z]{2,3}');
 if(regpattern.test(email))
 {
     console.log('Email is valid');
@@ -17,3 +17,5 @@ console.log('UC2-Checking the mandatory second part:');
 EmailCheck('abc@gmail');
 console.log('UC3-Checking the mandatory 3 part:');
 EmailCheck('abc@gmai.com');
+console.log('UC4-Checking the side part');
+EmailCheck('abc.xyx@gmai.com');
