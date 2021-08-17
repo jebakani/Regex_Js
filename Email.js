60e3e52e8495b2a31207234a368c82c76c046ae0
Jebakani <vjishwarya@gmail.com>
2021-08-17 21:52:19 +0530

    [JEBAKANI]Add:Checking the madatory third part

--- Email.js
+++ Email.js
@@ -1,7 +1,7 @@
 function EmailCheck(email)
 {
     //check mandatory first part
-let regpattern=RegExp('^[a-zA-Z]{3}@[a-zA-Z]+$');
+let regpattern=RegExp('^[a-zA-Z]{3}@[a-zA-Z]+\\.[a-z]{2,3}');
 if(regpattern.test(email))
 {
     console.log('Email is valid');
@@ -15,3 +15,5 @@ console.log('UC1-Checking mandatory condition:');
 EmailCheck('abc@gmail.com');
 console.log('UC2-Checking the mandatory second part:');
 EmailCheck('abc@gmail');
+console.log('UC3-Checking the mandatory 3 part:');
+EmailCheck('abc@gmai.com');
