8b473f21d84924d200d301612bef0a6336daa196
Jebakani <vjishwarya@gmail.com>
2021-08-17 21:31:13 +0530

    [JEBAKANI]Add: Checking the result when Pin code begin with alphabet

--- PinCode.js
+++ PinCode.js
@@ -1,6 +1,7 @@
-var prompt = require('prompt-sync')();
-let pincode=prompt('enter the pin code:');
+
 //pattern for pin code
+function PinCheck(pincode)
+{
 let regpattern=RegExp('^[1-9]{1}[0-9]{5}$');
 if(regpattern.test(pincode))
 {
@@ -10,3 +11,8 @@ else
 {
     console.log('pincode is invalid');
 }
+}
+console.log('UC1:checking for value 400088: ')
+PinCheck(400088);
+console.log('UC2 :Checking when the pincode begin with alphabet:')
+PinCheck('A400088');
